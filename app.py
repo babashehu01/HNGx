510e35516c27f74178d2468b17e9e3cc3e5417ac
babashehu01 <shettimababashehu57@gmail.com>
2023-09-10 00:12:25 +0100

    My information api endpoint

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/api', methods=['GET'])
+def api():
+    # user provided parameters
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    #date time parameters
+    current_day = datetime.now().strftime('%A')
+    utc_now = datetime.now().strftime('%Y-%m-%dT%XZ')
+    
+    #response parameters
+    response = {
+            "slack_name": slack_name,
+            "current_day": current_day,
+            "utc_time": utc_now,
+            "track": track,
+            "github_file_url": "https://github.com/babashehu01/HNGx/app.py",
+            "github_repo_url": "https://github.com/babashheu01/HNGx",
+            "status_code": 200
+    }
+
+    return jsonify(response)
